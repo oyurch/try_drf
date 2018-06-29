a36dc283578fbafa1d276c43660fbb2b1052102d
oyurch-ss <oyurch@softserveinc.com>
2018-06-29 11:57:49 +0300

    complite guide, chenge snippets urls with DefaulteRouter

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,34 +1,16 @@
-from django.urls import re_path, path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import include, path
 
-from snippets.views import SnippetViewSet, UserViewSet, api_root
-from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
+from snippets import views
 
-urlpatterns = format_suffix_patterns([
-    re_path(r'^$', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    re_path(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
-    re_path(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
-    path('users/', user_list, name='user-list'),
-    re_path(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
-])
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls))
+]
