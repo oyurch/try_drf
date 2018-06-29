60dfc958b249bbde2ec89e4492e5dd8584bf666e
oyurch-ss <oyurch@softserveinc.com>
2018-06-29 12:43:04 +0300

    add schema and coreapi

--- algolia/urls.py
+++ algolia/urls.py
@@ -17,9 +17,15 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title='Pastebin API')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('schema/', schema_view),
     path('', include('snippets.urls')),
     re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
